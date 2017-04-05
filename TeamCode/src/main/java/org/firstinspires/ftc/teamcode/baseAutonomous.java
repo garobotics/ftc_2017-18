5d97a944185fd89f6fc0abb618eff51166b31421
garobotics <robotics@greenwichacademy.org>
2017-04-05 15:53:32 -0400

    4/5/2017 3:53 - new base autonomous program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseAutonomous.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by ga on 4/5/17.
+ */
+
+public class baseAutonomous extends LinearOpMode {
+    //todo: make a working autonomous program lol
+
+
+}
