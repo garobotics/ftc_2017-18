45c4ff0313ee079a611bc2bc6738be9fca778764
garobotics <robotics@greenwichacademy.org>
2017-10-27 16:30:24 -0400

    10/27/2017 proof of concept

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseAutonomous.java
@@ -8,13 +8,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
 /**
  * Created by ga on 9/13/17.
  */
 
-@Autonomous(name="Base Autonomous")
+@Autonomous(name="THE Base Autonomous")
 
 public class baseAutonomous extends LinearOpMode {
 
@@ -27,6 +31,16 @@ public class baseAutonomous extends LinearOpMode {
     // hsvValues is an array that will hold the hue, saturation, and value information.
     float hsvValues[] = {0F,0F,0F};
     String ballColor;
+
+
+    HardwarePushbot robot   = new HardwarePushbot();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
 
     @Override
     public void runOpMode() {
@@ -54,6 +68,8 @@ public class baseAutonomous extends LinearOpMode {
         // reset encoder target positions to 0 on drive wheels
         wheelLF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         wheelRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wheelLB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wheelRB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         // set drive power to 0
         wheelRF.setPower(0.0);
@@ -66,21 +82,16 @@ public class baseAutonomous extends LinearOpMode {
 
         // METHODS GO HERE: THIS ACTS AS THE MAIN METHOD THAT CALLS ALL THE OTHER METHODS
 
-        driveEncoders(6000,"FORWARD");
-        driveEncoders(6000,"BACKWARD");
+        driveEncoders(0.5, 10, "forward", 5);
+        sleep(1000);
+        driveEncoders(0.5, 10, "backward", 5);
+        sleep(1000);
+        driveEncoders(0.5, 10, "left", 5);
+        sleep(1000);
+        driveEncoders(0.5, 10, "right", 5);
+        sleep(5000);
+        knockJewel("blue");
         //TODO: why does the drive encoders method work in isolation but not inside knockJewel method
-/*
-        telemetry.addData("1", String.format("YO IM IN AUTONOMOUS"));
-        telemetry.update();
-        sleep(1000);
-        setJewelArm("out");
-        telemetry.addData("1", String.format("YO I PUT MY ARM OUT"));
-        telemetry.update();
-        sleep(1000);
-        knockJewel("red");
-        telemetry.update();
-        sleep(1000);
-        */
         end();
 
     }
@@ -93,103 +104,128 @@ public class baseAutonomous extends LinearOpMode {
     }
 
 
-    public void driveEncoders(int distance, String direction) {
-        // options for distance = any integer (this is the target position)
-        // options for direction = FORWARD, BACKWARD, CRAB RIGHT, CRAB LEFT
+    public void driveEncoders(double speed,
+                             double distance, String direction,
+                             double timeoutS) {
+        int newTargetLB; // in ticks
+        int newTargetLF;
+        int newTargetRF;
+        int newTargetRB;
 
-        // reset encoders each time method is called
-        wheelLF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        wheelRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
 
-        telemetry.addData("1", String.format("YO I RESET THE ENCODERS"));
-        telemetry.update();
-        sleep(1000);
+            // Determine new target position, and pass to motor controller
+            // Convert inches to ticks
+            int inchesToTicks = (int)(distance * COUNTS_PER_INCH);
 
-        // change mode to run to position
-        wheelLF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        wheelRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            if (direction.equalsIgnoreCase("forward")) { // if direction is forward
+                // change target positions to go forward
+                newTargetLB = wheelLB.getCurrentPosition() - inchesToTicks;
+                newTargetLF = wheelLF.getCurrentPosition() - inchesToTicks;
+                newTargetRF = wheelRF.getCurrentPosition() - inchesToTicks;
+                newTargetRB = wheelRB.getCurrentPosition() - inchesToTicks;
 
-        // set the target position to the parameter passed in
+                // set all wheels to new target positions
+                wheelLB.setTargetPosition(newTargetLB);
+                wheelLF.setTargetPosition(newTargetLF);
+                wheelRF.setTargetPosition(newTargetRF);
+                wheelRB.setTargetPosition(newTargetRB);
+            }
+            else if (direction.equalsIgnoreCase("backward")) { // if direction is backward
+                // change target positions to go backward
+                newTargetLB = wheelLB.getCurrentPosition() + inchesToTicks;
+                newTargetLF = wheelLF.getCurrentPosition() + inchesToTicks;
+                newTargetRF = wheelRF.getCurrentPosition() + inchesToTicks;
+                newTargetRB = wheelRB.getCurrentPosition() + inchesToTicks;
 
+                // set all wheels to new target positions
+                wheelLB.setTargetPosition(newTargetLB);
+                wheelLF.setTargetPosition(newTargetLF);
+                wheelRF.setTargetPosition(newTargetRF);
+                wheelRB.setTargetPosition(newTargetRB);
+            }
+            else if (direction.equalsIgnoreCase("left")) { // if direction is left
+                // change target positions to go left
+                newTargetLB = wheelLB.getCurrentPosition() - inchesToTicks;
+                newTargetLF = wheelLF.getCurrentPosition() + inchesToTicks;
+                newTargetRF = wheelRF.getCurrentPosition() - inchesToTicks;
+                newTargetRB = wheelRB.getCurrentPosition() + inchesToTicks;
 
-        if (direction.equals("FORWARD")) {
+                // set all wheels to new target positions
+                wheelLB.setTargetPosition(newTargetLB);
+                wheelLF.setTargetPosition(newTargetLF);
+                wheelRF.setTargetPosition(newTargetRF);
+                wheelRB.setTargetPosition(newTargetRB);
+            }
+            else if (direction.equalsIgnoreCase("right")) { // if direction is right
+                // change target positions to go right
+                newTargetLB = wheelLB.getCurrentPosition() + inchesToTicks;
+                newTargetLF = wheelLF.getCurrentPosition() - inchesToTicks;
+                newTargetRF = wheelRF.getCurrentPosition() + inchesToTicks;
+                newTargetRB = wheelRB.getCurrentPosition() - inchesToTicks;
 
-            wheelLF.setTargetPosition(-distance);
-            wheelRF.setTargetPosition(-distance);
-
-            wheelLF.setPower(-0.5);
-            wheelRF.setPower(-0.5);
-            wheelLB.setPower(-0.5);
-            wheelRB.setPower(-0.5);
-
-
-            // check to see if robot is done yet
-            while (wheelLF.getCurrentPosition() > -distance && wheelRF.getCurrentPosition() > -distance) {
-                telemetry.addData("0", String.format("Moving forward, please stand by..."));
-                telemetry.addData("1", wheelLF.getCurrentPosition());
-                telemetry.addData("2", wheelRF.getCurrentPosition());
+                // set all wheels to new target positions
+                wheelLB.setTargetPosition(newTargetLB);
+                wheelLF.setTargetPosition(newTargetLF);
+                wheelRF.setTargetPosition(newTargetRF);
+                wheelRB.setTargetPosition(newTargetRB);
+            }
+            else {
+                stop();
+                telemetry.addData("4", String.format("BRO U DIDN'T PASS IN A VALID DIRECTION IM NOT MOVING LOLOL"));
                 telemetry.update();
             }
 
-        }
-        else if (direction.equals("BACKWARD")) {
-            wheelLF.setTargetPosition(distance);
-            wheelRF.setTargetPosition(distance);
+            // Turn On RUN_TO_POSITION
+            wheelLF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelLB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelRB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            wheelLF.setPower(0.5);
-            wheelRF.setPower(0.5);
-            wheelLB.setPower(0.5);
-            wheelRB.setPower(0.5);
+            // this is the absolute value because setPower works differently with encoders
+            wheelLF.setPower(Math.abs(speed));
+            wheelRF.setPower(Math.abs(speed));
+            wheelRB.setPower(Math.abs(speed));
+            wheelLB.setPower(Math.abs(speed));
+
+            // reset the timeout time and start motion.
+            runtime.reset();
 
 
-            // check to see if robot is done yet
-            while (wheelLF.getCurrentPosition() < distance && wheelRF.getCurrentPosition() < distance) {
-                telemetry.addData("1", String.format("Moving backward, please stand by..."));
-                telemetry.update();
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (wheelLF.isBusy() && wheelRF.isBusy() && wheelLB.isBusy() && wheelRB.isBusy())) {
+
+                // Display it for the driver.
+                /*telemetry.addData("Path1",  "Running to %7d:" , distance); // distance wheels should go
+                // show the current position of all four wheels
+                telemetry.addData("Path2",  "Currently at %7d, %7d, %7d, %7d",
+                        wheelLF.getCurrentPosition(),
+                        wheelRF.getCurrentPosition(),
+                        wheelRB.getCurrentPosition(),
+                        wheelLB.getCurrentPosition());
+                telemetry.update(); */
             }
 
-
-        }
-        else if (direction.equals("CRAB LEFT")) { //
-            wheelLF.setTargetPosition(-distance);
-            wheelRF.setTargetPosition(distance);
-
-            wheelRF.setPower(0.5);
-            wheelRB.setPower(-0.5);
-            wheelLF.setPower(-0.5);
-            wheelLB.setPower(0.5);
-
-            // check to see if robot is done yet
-            while (wheelLF.getCurrentPosition() > -distance || wheelRF.getCurrentPosition() < distance) {
-                telemetry.addData("2", String.format("Crabbing left, please stand by..."));
-                telemetry.update();
-            }
-        }
-        else if (direction.equals("CRAB RIGHT")) {
-
-            wheelLF.setTargetPosition(distance);
-            wheelRF.setTargetPosition(-distance);
-
-            wheelRF.setPower(-0.5);
-            wheelRB.setPower(0.5);
-            wheelLF.setPower(0.5);
-            wheelLB.setPower(-0.5);
-
-            // check to see if robot is done yet
-            while (wheelLF.getCurrentPosition() < distance && wheelRF.getCurrentPosition() > -distance) {
-                telemetry.addData("3", String.format("Crabbing right, please stand by..."));
-                telemetry.update();
-            }
-        }
-        else {
+            // Stop all motion
             wheelLF.setPower(0);
             wheelRF.setPower(0);
-            wheelLB.setPower(0);
             wheelRB.setPower(0);
-            telemetry.addData("4", String.format("YOU DIDN'T PASS IN A VALID DIRECTION. I'M NOT MOVING. LOLZ"));
-            telemetry.update();
+            wheelLB.setPower(0);
+/*
+            // Turn off RUN_TO_POSITION
+            wheelLF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelRF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+*/
+            //  sleep(250);   // optional pause after each move
         }
-
     }
 
 
@@ -220,12 +256,12 @@ public class baseAutonomous extends LinearOpMode {
         // if the jewel is equal to the targetColor, drive forward to knock it off
         String jewelColor = convertHueToString();
         if (targetColor.equals(jewelColor)){
-            driveEncoders(100, "FORWARD");
+            driveEncoders(0.5, 1, "forward", 1);
             telemetry.addData("4", String.format("I see the opponent's color. Driving forward..."));
         }
         // otherwise, if it's the opposite color, go backward
         else if (!jewelColor.equals("other")) {
-            driveEncoders(100, "BACKWARD");
+            driveEncoders(0.5, 1, "backward", 1);
             telemetry.addData("4", String.format("I see your team's color. Driving backward..."));
         }
         else { // it's "other"
